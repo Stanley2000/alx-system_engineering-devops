fe56d412dccf83c9a9f5b2933b7f1065724979bd
stanley agbo <stanleykelechi2000@gmail.com>
2023-07-06 15:57:00 +0100

    added a new file

--- 0x01-shell_permissions/commit.sh
+++ 0x01-shell_permissions/commit.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/bash
-echo "Enter the file you want to give execute command"
-read $filename
-chmod +x $filename
 git add .
-git commit -m "added a new file, $filename"
+git commit -m "added a new file"
 git push
 
